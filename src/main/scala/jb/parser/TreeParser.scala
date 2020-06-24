aa5a4b5adc1cd7b5f580e5fe31c5b551ef6064b0
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-06-24 04:00:29 +0200

    Add new tree parser

--- src/main/scala/jb/parser/TreeParser.scala
+++ src/main/scala/jb/parser/TreeParser.scala
@@ -1,54 +1,32 @@
 package jb.parser
 
-import jb.model._
-import org.apache.spark.ml.tree.{ContinuousSplit, InternalNode, Node}
+import jb.model.Cube
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.tree.{ContinuousSplit, InternalNode, LeafNode, Node}
 
-class TreeParser() {
+class TreeParser {
 
-  def dt2rect(parent: Rect, node: Node): Array[Rect] = {
-    node match {
-      case _: InternalNode =>
-        val interNode = node.asInstanceOf[InternalNode]
-
-        val newMax = parent.max.clone()
-        newMax(interNode.split.featureIndex) = interNode.split.asInstanceOf[ContinuousSplit].threshold
-        val newMin = parent.min.clone()
-        newMin(interNode.split.featureIndex) = interNode.split.asInstanceOf[ContinuousSplit].threshold
-
-        val leftChild = parent.copy(max = newMax)
-        val rightChild = parent.copy(min = newMin)
-
-        dt2rect(leftChild, node.asInstanceOf[InternalNode].leftChild) ++ dt2rect(rightChild, node.asInstanceOf[InternalNode].rightChild)
-      case _ =>
-        Array(parent.copy(label = node.prediction))
+  def extractCutpointsRecursively(tree: Node): List[Tuple2[Int, Double]] = {
+    tree match {
+      case leaf: LeafNode => List()
+      case branch: InternalNode => branch.split match {
+        case contSplit: ContinuousSplit => {
+          (extractCutpointsRecursively(branch.leftChild)
+            ::: ((contSplit.featureIndex -> contSplit.threshold)
+            :: extractCutpointsRecursively(branch.rightChild)))
+        }
+        case _ => throw Exception("Unsupported split")
+      }
+      case _ => throw Exception("Unsupported node")
     }
   }
 
-  def rects2edges(rects: Array[Rect]): Array[Edge] = {
-    val indices = for (i <- rects.indices; j <- rects.indices if i < j) yield (i, j)
-    indices.map(tuple => (rects(tuple._1), rects(tuple._2))).filterNot(areOfSameClasses).filter(areAdjacent).map(createEdge).toArray
+  def extractCutpoints(trees: List[DecisionTreeClassificationModel]): Unit = {
+    trees.flatMap(extractCutpointsTree)
   }
 
-  def areAdjacent(tuple: (Rect, Rect)): Boolean = {
-    val (r1, r2) = tuple
-    for (dim <- r1.min.indices) {
-      if (math.max(r1.min(dim), r2.min(dim)) > math.min(r1.max(dim), r2.max(dim))) return false
-    }
-    true
-  }
-
-  def areOfSameClasses(tuple: (Rect, Rect)): Boolean = {
-    tuple._1.label == tuple._2.label
-  }
-
-  def createEdge(tuple: (Rect, Rect)): Edge = {
-    val (r1, r2) = tuple
-    val (mins, maxes) = (new Array[Double](r1.min.length), new Array[Double](r1.min.length))
-    for (dim <- r1.min.indices) {
-      mins(dim) = math.max(r1.min(dim), r2.min(dim))
-      maxes(dim) = math.min(r1.max(dim), r2.max(dim))
-    }
-    Edge(mins, maxes)
+  private def extractCutpointsTree(tree: DecisionTreeClassificationModel): List[Cube] = {
+    extractCutpointsRecursively(tree.rootNode, Array())
   }
 
 }
