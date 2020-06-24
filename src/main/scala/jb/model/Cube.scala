25da6e8591cdabc6dbb7799f920e4c58d6e4ef09
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-06-24 03:59:15 +0200

    Extend Cube model

--- src/main/scala/jb/model/Cube.scala
+++ src/main/scala/jb/model/Cube.scala
@@ -1,3 +1,7 @@
 package jb.model
 
-case class Cube(var min: Array[Double], var max: Array[Double])
+case class Cube(min: Array[Double], max: Array[Double])
+
+case class CountingCube(min: Array[Double], max: Array[Double], labelCount: Map[Int, Int]) extends Cube(min, max)
+
+case class LabelledCube(min: Array[Double], max: Array[Double], label: Int) extends Cube(min, max)
