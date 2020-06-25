3340a0e61a1f0a3c4f6e3057898986b19243df17
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-06-25 21:24:41 +0200

    Refactor

--- src/main/scala/jb/Runner.scala
+++ src/main/scala/jb/Runner.scala
@@ -4,18 +4,15 @@ import java.util.stream.IntStream
 
 import jb.conf.Config
 import jb.io.FileReader.getRawInput
-import jb.parser.TreeParser1
 import jb.prediction.Predictions.predictBaseClfs
 import jb.selector.FeatureSelectors
 import jb.server.SparkEmbedded
-import jb.tester.FullTester.{testI, testMv, testRF}
+import jb.tester.FullTester.{testMv, testRF}
 import jb.util.Const._
 import jb.util.Util._
-import jb.util.functions.DistMappingFunctions._
-import jb.util.functions.WeightAggregators.sumOfWeights
 import jb.vectorizer.FeatureVectorizers.getFeatureVectorizer
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
+import org.apache.spark.ml.classification.DecisionTreeClassifier
 
 class Runner(val nClassif: Int, var nFeatures: Int) {
 
@@ -36,6 +33,7 @@ class Runner(val nClassif: Int, var nFeatures: Int) {
     input = optimizeInput(input, dataPrepModel)
 
     val (mins, maxes) = getExtrema(input, getSelectedFeatures(dataPrepModel))
+    //TODO normalize
 
     val nSubsets = nClassif + 1
     val subsets = input.randomSplit(IntStream.range(0, nSubsets).mapToDouble(_ => 1D / nSubsets).toArray)
@@ -57,38 +55,19 @@ class Runner(val nClassif: Int, var nFeatures: Int) {
     val mvQualityMeasure = testMv(testedSubset, nClassif)
     val rfQualityMeasure = testRF(trainingSubset, testSubset, nClassif)
 
-    val edges: Array[Array[Edge]] = if (coefficients.edgeDependent) getEdges(mins, maxes, baseModels) else null
-    // TODO: moments - bottleneck
-    // TODO: batch by moments calculation method
-    val moments = if (coefficients.momentDependent) mappingModel match {
-      case PreTraining() => calculateMomentsByLabels(input, getSelectedFeatures(dataPrepModel))
-      //      case PostTrainingCV() => calculateMomentsByPredictionCollectively(cvSubset, getSelectedFeatures(dataPrepModel), baseModels, false)
-      case PostTrainingTrain() => calculateMomentsByPredictionRespectively(trainingSubsets, getSelectedFeatures(dataPrepModel), baseModels, false)
-      case PostTrainingAll() => calculateMomentsByPredictionCollectively(input, getSelectedFeatures(dataPrepModel), baseModels, false)
-      //      case PostTrainingCVFiltered() => calculateMomentsByPredictionCollectively(cvSubset, getSelectedFeatures(dataPrepModel), baseModels, true)
-      case PostTrainingTrainFiltered() => calculateMomentsByPredictionRespectively(trainingSubsets, getSelectedFeatures(dataPrepModel), baseModels, true)
-      case PostTrainingAllFiltered() => calculateMomentsByPredictionCollectively(input, getSelectedFeatures(dataPrepModel), baseModels, true)
-    } else null
-    val integratedModel =
-      if (coefficients.onlyEdgeDependent) new EdgeIntegratedDecisionTreeModel(baseModels, singleMapping(coefficients), sumOfWeights, edges)
-      else if (coefficients.onlyMomentDependent) new MomentIntegratedDecisionTreeModel(baseModels, singleMapping(coefficients), sumOfWeights, moments)
-      else new CombinedIntegratedDecisionTreeModel(baseModels, composedMapping(coefficients), sumOfWeights, edges, moments)
-    val iPredictions = integratedModel.transform(testedSubset)
-    val iQualityMeasure = testI(iPredictions, testedSubset)
+    //    val integratedModel =
+    //      if (coefficients.onlyEdgeDependent) new EdgeIntegratedDecisionTreeModel(baseModels, singleMapping(coefficients), sumOfWeights, edges)
+    //      else if (coefficients.onlyMomentDependent) new MomentIntegratedDecisionTreeModel(baseModels, singleMapping(coefficients), sumOfWeights, moments)
+    //      else new CombinedIntegratedDecisionTreeModel(baseModels, composedMapping(coefficients), sumOfWeights, edges, moments)
+    //    val iPredictions = integratedModel.transform(testedSubset)
+    //    val iQualityMeasure = testI(iPredictions, testedSubset)
 
     clearCache(subsets)
 
     Array(mvQualityMeasure._1, if (mvQualityMeasure._2.isNaN) 0D else mvQualityMeasure._2, mvQualityMeasure._3, mvQualityMeasure._4,
-      rfQualityMeasure._1, if (rfQualityMeasure._2.isNaN) 0D else rfQualityMeasure._2, rfQualityMeasure._3, rfQualityMeasure._4,
-      iQualityMeasure._1, if (iQualityMeasure._2.isNaN) 0D else iQualityMeasure._2, iQualityMeasure._3, iQualityMeasure._4)
+      rfQualityMeasure._1, if (rfQualityMeasure._2.isNaN) 0D else rfQualityMeasure._2, rfQualityMeasure._3, rfQualityMeasure._4)
+    //      iQualityMeasure._1, if (iQualityMeasure._2.isNaN) 0D else iQualityMeasure._2, iQualityMeasure._3, iQualityMeasure._4)
 
-  }
-
-  private def getEdges(mins: Array[Double], maxes: Array[Double], baseModels: Array[DecisionTreeClassificationModel]): Array[Array[Edge]] = {
-    val rootRect = Rect(mins, maxes)
-    val treeParser = new TreeParser1()
-    val rects = baseModels.map(model => treeParser.dt2rect(rootRect, model.rootNode))
-    rects.map(treeParser.rects2edges)
   }
 
 }
