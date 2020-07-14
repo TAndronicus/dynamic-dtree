29593e0d11825ac452760ac2e6d613fed2b7557f
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-07-14 07:13:04 +0200

    Refactor

--- src/main/scala/jb/ExperimentPlan.scala
+++ src/main/scala/jb/ExperimentPlan.scala
@@ -6,13 +6,8 @@ object ExperimentPlan {
 
   def main(args: Array[String]): Unit = {
     SparkEmbedded.setLogError()
-    val nClassifs = Array(3, 5, 7, 9)
+    val nClassifs = Array(7, 9)
     val nFeatures = 2
-    val alphas = Array(0, .3, .7, 1)
-    val betas = Array(0, .5)
-    val gammas = Array(5, 20)
-    //    val (beta1, beta2) = (.5, 0)
-    //    val (gamma1, gamma2) = (20, 5)
 
     for (nC <- nClassifs) {
       MultiRunner.run(nC, nFeatures)
